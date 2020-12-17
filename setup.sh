fb818d769855bbcf05679aa4fd09fc133ac5ab1a
Max Andersson <max.andersson@neotechnology.com>
2020-12-17 14:56:22 +0100

    updated

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git submodule update --init
-sudo apt install python3-pip -y
+#sudo apt install python3-pip -y
 sudo pip3 install virtualenv 
 virtualenv venv 
 virtualenv -p $(which python3) venv
